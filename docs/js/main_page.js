30f9f72d34d8c5f0f106c79fde6022bfbc9e6dab
CengizhanParlak <cengizhan.parlak@hotmail.com>
2024-07-08 03:43:29 +0300

    update: main page layout add: blog section

---
+++ docs/js/main_page.js
@@ -0,0 +1,71 @@
+const enabledClass = "enabled";
+const disabledClass = "disabled";
+const selectedClass = "selected";
+let lastSection;
+let lastButton;
+
+function addAfterContentLoadListener(callback) {
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", callback);
+  } else {
+    callback();
+  }
+}
+
+function initFirstValues() {
+  lastButton = document.getElementById("experience-button");
+  lastSection = document.getElementById("experience-summary");
+}
+
+function addButtonListeners() {
+  const expButton = document.getElementById("experience-button");
+  const projButton = document.getElementById("projects-button");
+  const blogButton = document.getElementById("blog-button");
+  const blogSection = document.getElementById("blog-posts-summary");
+  const projSection = document.getElementById("projects-summary");
+  const expSection = document.getElementById("experience-summary");
+
+  expButton.addEventListener("click", function () {
+    enableSection(expButton, expSection);
+  });
+  projButton.addEventListener("click", function () {
+    enableSection(projButton, projSection);
+  });
+  blogButton.addEventListener("click", function () {
+    enableSection(blogButton, blogSection);
+  });
+}
+
+function enableSection(button, section) {
+  disableLastSection();
+  disableButton();
+  section.classList.add(enabledClass);
+  section.classList.remove(disabledClass);
+  colorButton(button);
+  assignLastValues(button, section);
+}
+
+function colorButton(button) {
+  button.classList.add(selectedClass);
+}
+
+function assignLastValues(button, section) {
+  lastButton = button;
+  lastSection = section;
+}
+
+function disableButton() {
+  if (lastButton) {
+    lastButton.classList.remove(selectedClass);
+  }
+}
+
+function disableLastSection() {
+  if (lastSection) {
+    lastSection.classList.add(disabledClass);
+    lastSection.classList.remove(enabledClass);
+  }
+}
+
+initFirstValues();
+addAfterContentLoadListener(addButtonListeners);
